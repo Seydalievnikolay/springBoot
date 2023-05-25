a9984447a83bd33cbee8697aac016bb1cc06a2d5
Seydalievnikolay <nikolay-seydalie@yandex.ru>
2023-05-25 12:48:38 +0700

    first commit

---
+++ src/main/java/ru/skypro/lessons/springboot/EmployeeApplication/EmployeeApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.EmployeeApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+}
